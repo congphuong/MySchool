932aa497f763a7a5f796b9ffbcc0af9b92374d5e
congphuong <congphuong32@gmail.com>
2017-06-21 02:45:28 +0700

    update

--- src/components/navigations/TabGroup.js
+++ src/components/navigations/TabGroup.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Group from '../screens/Group';
+import NewTopic from '../screens/NewTopic';
 
 const TabGroup = StackNavigator({
     GroupScreen: {
         screen: Group,
+        navigationOptions: {
+            tabBarLabel: 'Group',
+            tabBarIcon: ({ tintColor }) =>
+                <Image
+                    source={require('../img/Group_Black.png')}
+                    style={[styles.icon, { tintColor }]}
+                />,
+        }
+    },
+    NewTopic: {
+        screen: NewTopic,
         navigationOptions: {
             tabBarLabel: 'Group',
             tabBarIcon: ({ tintColor }) =>
--- src/components/navigations/TabNotif.js
+++ src/components/navigations/TabNotif.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Notifications from '../screens/Notifications';
+import NewNotification from '../screens/NewNotification';
 
 const TabNotif = StackNavigator({
     NotifScreen: {
         screen: Notifications,
+        navigationOptions: {
+            tabBarLabel: 'Notification',
+            tabBarIcon: ({ tintColor }) =>
+                <Image
+                    source={require('../img/Notif_Black.png')}
+                    style={[styles.icon, { tintColor }]}
+                />,
+        }
+    },
+    NewNotification: {
+        screen: NewNotification,
         navigationOptions: {
             tabBarLabel: 'Notification',
             tabBarIcon: ({ tintColor }) =>
